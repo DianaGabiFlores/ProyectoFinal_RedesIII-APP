fd0df2ad2bd171dcc188a359427d40f17cb12528
GusGit2003 <gusavegue@gmail.com>
2024-11-30 10:46:26 -0600

    Avance de Registrar Usuario, pendiente NodeJS

--- ProyectoRedes/app/src/main/java/com/dgfp/proyectoredes/APIServiceUsuario.kt
+++ ProyectoRedes/app/src/main/java/com/dgfp/proyectoredes/APIServiceUsuario.kt
@@ -1,9 +1,14 @@
 package com.dgfp.proyectoredes
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface APIServiceUsuario {
     @GET("usuarios")
     fun getPostsUsuarios(): Call<List<Usuario>>
+
+    @POST("usuarios")
+    fun crearUsuario(@Body usuario: Usuario): Call<Usuario>
 }
